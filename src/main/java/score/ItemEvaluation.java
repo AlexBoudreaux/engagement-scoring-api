7a23e5ce902891f2b14078549a5c444bf7d0d8d1
Boosey <john.boudreaux@us.ibm.com>
2021-11-12 11:19:19 -0600

    ItemEvaluation Initial

---
+++ src/main/java/score/ItemEvaluation.java
@@ -0,0 +1,20 @@
+package score;
+
+import java.util.List;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+public class ItemEvaluation extends PanacheEntity {
+
+  @ManyToOne
+  @JoinColumn(name = "FK_TemplateId")
+  public Template template;
+
+  @ManyToMany
+  public List<PossibleResponse> selectedResponses;
+  
+}
